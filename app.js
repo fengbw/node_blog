58db4d2e9fffd8a6a1ad41275f42d2ae173635cf
fengbw <fengbwseu@163.com>
2020-10-28 22:19:03 -0400

    front-end

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-var express = require('express')
-var path = require('path')
+const express = require('express')
+const path = require('path')
 
-var app = express()
+const app = express()
 
-app.use('/public/', express.static(path.join(__dirname, './public/')))
-app.use('/node_modules/', express.static(path.join(__dirname, './node_modules/')))
+app.use('/public/', express.static(path.join(__dirname, 'public')))
+app.use('/node_modules/', express.static(path.join(__dirname, 'node_modules')))
+
+app.engine('html', require('express-art-template'))
+app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', function (req, res) {
-    res.send('hello')
+    res.render('index.html')
 })
 
 app.listen(5000, function () {
